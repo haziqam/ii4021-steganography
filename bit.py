88b74f36ad06ed53c212e5343257c7afb6dda944
Haziq Abiyyu Mahdy <haziqabiyyu@gmail.com>
2025-03-14 23:12:15 +0700

    fix: set_bit and set_bit_at_index, rfc: add Bit type

--- bit.py
+++ bit.py
@@ -1,16 +1,34 @@
-def get_lsb(n: int) -> int:
+from typing import Literal
+
+
+type Bit = Literal[0, 1]
+
+def get_lsb(n: int) -> Bit:
     return n & 1
 
-def set_lsb(n: int, bit: int) -> int:
-    return (n & 0b11111110) | bit
+def set_lsb(n: int, bit: int) -> Bit:
+    bit = bit & 1
+    return (n & ~1) | bit
 
 def get_bit_at_index(n: int, index: int) -> int:
+    """
+    Note: Index 0: LSB
+    """
     mask = 1 << index
     return (n & mask) >> index
 
-def set_bit_at_index(n: int, index: int, bit: int) -> int:
-    mask = bit << index
-    return n | mask
+def set_bit_at_index(n: int, index: int, bit: Bit) -> int:
+    """
+    Note: Index 0: LSB
+    """
+    bit = bit & 1
+
+    if bit == 1:
+        mask = 1 << index
+        return n | mask
+    
+    mask = ~(1 << index)
+    return n & mask
 
 if __name__ == '__main__':
     test = 0b10001001
