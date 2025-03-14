28a07cb538f62f41c1ed855bc829ba28481e5b39
Haziq Abiyyu Mahdy <haziqabiyyu@gmail.com>
2025-03-14 23:13:40 +0700

    feat: store message length in image, rfc: change lsb.py to image_lsb.py, fix: random position embed

--- image_lsb.py
+++ image_lsb.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from bit import get_lsb, set_lsb, get_bit_at_index, set_bit_at_index
+from bit import Bit, get_lsb, set_lsb, get_bit_at_index, set_bit_at_index
 
 from utils import create_sequence
 
@@ -9,12 +9,12 @@ from utils import create_sequence
 # 3) Explore case image tidak RGB (grayscale ato item putih)
 # 3) Explore cara nyimpen message length di image
 
-def set_lsb_at_position(pixels, x: int, y: int, message_bit: int):
+def set_lsb_at_position(pixels, x: int, y: int, message_bit: Bit):
     r, g, b = pixels[x, y]
     b = set_lsb(b, message_bit)
     pixels[x, y] = (r, g, b)
 
-def get_lsb_at_position(pixels, x: int, y: int) -> int:
+def get_lsb_at_position(pixels, x: int, y: int) -> Bit:
     _, _, b = pixels[x, y]
     return get_lsb(b)
 
@@ -22,6 +22,11 @@ def embed_message(image: Image, message: str, sequence: list[int]) -> Image:
     pixels = image.load()
     width, _ = image.size
 
+    # Prepend message length to message (as an 8-bit char)
+    message_length_in_byte = chr(len(message))
+    message = message_length_in_byte + message
+
+    # Embed message
     for idx, c in enumerate(message):
         for i in range(8):
             target_idx = sequence[idx * 8 + i]
@@ -32,12 +37,22 @@ def embed_message(image: Image, message: str, sequence: list[int]) -> Image:
 
     return image
 
-def extract_message(image: Image, message_length: int, sequence: list[int]) -> str:
+def extract_message(image: Image, sequence: list[int]) -> str:
     pixels = image.load()
     width, _ = image.size
 
+    # Extract message length (first 8 bits)
+    message_length = 0
+    for i in range(8):
+        target_idx = sequence[i]
+        x = target_idx % width
+        y = target_idx // width
+        message_bit = get_lsb_at_position(pixels, x, y)
+        message_length = set_bit_at_index(message_length, i, message_bit)
+
+    # Extract message
     message: list[str] = []
-    for idx in range(message_length):
+    for idx in range(1, message_length + 1):
         current_byte = 0
         for j in range(8):
             target_idx = sequence[idx * 8 + j]
@@ -56,8 +71,8 @@ if __name__ == "__main__":
     message = "secretmessage"
     seed = 3
 
-    sequence = create_sequence(len(message) * 8, seed)
+    sequence = create_sequence((len(message) + 1) * 8, image.width * image.height, seed)
 
     embedded_image = embed_message(image, message, sequence)
 
-    print(extract_message(embedded_image, len(message), sequence))
+    print(extract_message(embedded_image, sequence))
